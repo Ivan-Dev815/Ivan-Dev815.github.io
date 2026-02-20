70e485b771f84d7a1badf38309368a595dcb8317
ivanpimienta <ivanpimienta@MacBook-Pro-de-soportemultiplica.local>
2026-02-20 17:13:52 -0500

    v1.2 - Nuevo icono actualizado

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 self.addEventListener('install', e => {
     e.waitUntil(
-        caches.open('prestamos-v1.2').then(cache => {
+        caches.open('prestamos-v1.3').then(cache => {
             return cache.addAll([
                 './',
                 './index.html',
                 './styles.css',
                 './app.js',
                 './manifest.json',
-                './icons/icon_v2.png'
+                './icons/icon_v1.3.png'
             ]);
         })
     );
