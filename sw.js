cb3a27d338625acdda8d31183970be223409f90e
Ivan Pimienta <pimientaivan54@gmail.com>
2026-04-03 11:08:17 -0500

    Buscador version 1.5

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', e => {
     e.waitUntil(
-        caches.open('prestamos-v1.4').then(cache => {
+        caches.open('prestamos-v1.5').then(cache => {
             return cache.addAll([
                 './',
                 './index.html',
